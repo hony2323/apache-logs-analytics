390b44d5e51bccfc1f40ffb12253271c651793ee
H E <hony2323@gmail.com>
2026-03-10 14:27:02 +0200

    Added IAnalysisController to the interfaces

--- core/interfaces.py
+++ core/interfaces.py
@@ -1,6 +1,16 @@
 from abc import ABC, abstractmethod
 
 
+class IAnalysisController(ABC):
+    @abstractmethod
+    def __init__(self, logs_input: ILogsInput, logs_parser: ILogsParser, logs_converter: ILogsConverter,
+                 logs_analyzer: ILogsAnalyzer, analysis_output: IAnalysisOutput):
+        pass
+    @abstractmethod
+    def start(self) -> None:
+        pass
+
+
 class ILogsInput(ABC):
     @abstractmethod
     def read_next(self, num_of_lines: int) -> list[str]:
@@ -27,6 +37,7 @@ class ILogsAnalyzer(ABC):
     @abstractmethod
     def add_to_analysis(self, converted_logs: list[ConvertedLogObject]) -> None:
         pass
+
     @abstractmethod
     def get_analysis(self) -> AnalyzedLogObject:
         pass
