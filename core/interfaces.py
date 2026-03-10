3eeda84e104360636c6ad910f1dd102ca9c48837
H E <hony2323@gmail.com>
2026-03-10 14:22:21 +0200

    Added core interfaces

---
+++ core/interfaces.py
@@ -0,0 +1,50 @@
+from abc import ABC, abstractmethod
+
+
+class ILogsInput(ABC):
+    @abstractmethod
+    def read_next(self, num_of_lines: int) -> list[str]:
+        pass
+
+    @abstractmethod
+    def is_finished(self) -> bool:
+        pass
+
+
+class ILogsParser(ABC):
+    @abstractmethod
+    def parse(self, raw_logs: list[str]) -> list[ParsedLogObject]:
+        pass
+
+
+class ILogsConverter(ABC):
+    @abstractmethod
+    def convert(self, parsed_logs: ParsedLogObject) -> list[ConvertedLogObject]:
+        pass
+
+
+class ILogsAnalyzer(ABC):
+    @abstractmethod
+    def add_to_analysis(self, converted_logs: list[ConvertedLogObject]) -> None:
+        pass
+    @abstractmethod
+    def get_analysis(self) -> AnalyzedLogObject:
+        pass
+
+
+class IAnalysisOutput(ABC):
+    @abstractmethod
+    def submit(self, analysis: AnalyzedLogObject) -> None:
+        pass
+
+
+class ParsedLogObject(ABC):
+    pass
+
+
+class ConvertedLogObject(ABC):
+    pass
+
+
+class AnalyzedLogObject(ABC):
+    pass
